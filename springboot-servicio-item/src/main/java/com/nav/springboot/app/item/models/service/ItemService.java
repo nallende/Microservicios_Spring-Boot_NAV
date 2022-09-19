ed1dce46f508391e7cb124a0c002ba2fcd890c7d
nav <nallende@neosoporte.cl>
2022-09-19 12:14:23 -0300

    Initial Commit 19th Sept

---
+++ springboot-servicio-item/src/main/java/com/nav/springboot/app/item/models/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.nav.springboot.app.item.models.service;
+
+import java.util.List;
+
+import com.nav.springboot.app.item.models.Item;
+
+public interface ItemService {
+	
+	public List<Item> findAll();
+	public Item findById(Long id, Integer cantidad);
+
+}
